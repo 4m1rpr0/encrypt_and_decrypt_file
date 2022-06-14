946dbc63e2d8593a8e634bdffa3456563c5c29ee
4m1rpr0 <sadeghyali13467@gmail.com>
2022-06-14 05:13:15 -0400

    Use password in encryption

--- encrypt and decrypt file.py
+++ encrypt and decrypt file.py
@@ -1,10 +1,13 @@
 import os
-def crypto(path, check):
+def crypto(path, check, password):
+    xor = 0
     if os.path.exists(path):
         with open(path, "rb") as file:
             with open(os.getcwd() + "/" + check, "wb") as file1:
                 for i in file.read():
-                    i = i ^ ord("b")
+                    for j in password:
+                        xor ^= ord(j)
+                    i ^= xor
                     file1.write(i.to_bytes(1, "big"))
         print("success")
     else:
@@ -13,9 +16,9 @@ print("Started Program\n1 - Encrypt File\n2 - Decrypt File\n3 - Exit")
 while True:
     check = input(">>> ")
     if check == "1":
-        crypto(input("Enter The File Path (Encrypt File): "), "Encrypt")
+        crypto(input("Enter The File Path (Encrypt File) : "), "Encrypt", input("Enter password : "))
     elif check == "2":
-        crypto(input("Enter The File Path (Decrypt File) : "), "Decrypt")
+        crypto(input("Enter The File Path (Decrypt File) : "), "Decrypt", input("Enter password : "))
     elif check == "3":
         print("bye :)")
         break
